e2d1467be653ef97c05a2b86726a2f54c8d31bd5
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-03-07 16:12:39 -0500

    Randomly launch the fruit prefab.

---
+++ Fruit Ninja/Assets/Script/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour 
+{
+	private List<Fruit> fruit = new List<Fruit>();
+	public GameObject fruitPrefab;
+
+	private float lastSpawn;
+	private float deltaSpawn = 2.0f;
+
+	private Fruit GetFruit()
+	{
+		Fruit f = fruit.Find(x => !x.IsActive); // find the false IsActive fruit
+
+		if (f == null)
+		{
+			f = Instantiate(fruitPrefab).GetComponent<Fruit>();
+			fruit.Add(f);
+		}
+
+		return f;
+	}
+
+	void Start () {
+		
+	}
+	
+	private void Update () 
+	{
+		if (Time.time - lastSpawn > deltaSpawn)
+		{
+			Fruit f = GetFruit();
+			float randomX = Random.Range(-1.5f, 1.5f);
+
+			f.LaunchFruit(Random.Range(1.5f, 3.0f), randomX, -randomX);
+			// A fruit is randomly spawn on the left or right or the middle of the screen
+			// if left throw to the right, right throw to the left, and middle straight up  
+
+			lastSpawn = Time.time;
+		}	
+	}
+}
