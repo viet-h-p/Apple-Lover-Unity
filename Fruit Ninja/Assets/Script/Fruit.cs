053b995b141fa65b0547f5d650e062d6ae406e87
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-04-03 21:11:59 -0400

    Create an animation when one fruit is sliced by using multiple sprites.

--- Fruit Ninja/Assets/Script/Fruit.cs
+++ Fruit Ninja/Assets/Script/Fruit.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Fruit : MonoBehaviour 
 {
@@ -7,7 +8,13 @@ public class Fruit : MonoBehaviour
 
 	private float verticalVelocity;
 	private float speed;
-	private bool isSliced;
+	private bool isSliced = false;
+	public SpriteRenderer spriteRenderer;
+
+	public Sprite[] sprites;
+	private int spriteIndex;
+	private float lastSpriteUpdate;
+	private float spriteUpdateDelta = 0.2f;
 
 	public void LaunchFruit(float verticalVelocity, float xSpeed, float xStart)
 	{
@@ -16,6 +23,8 @@ public class Fruit : MonoBehaviour
 		this.verticalVelocity = verticalVelocity;
 		transform.position = new Vector3(xStart, 0, 0);
 		isSliced = false;
+		spriteIndex = 0;
+		spriteRenderer.sprite = sprites[spriteIndex];
 	}
 
 	private void Update()
@@ -24,6 +33,16 @@ public class Fruit : MonoBehaviour
 
 		verticalVelocity -= GRAVITY * Time.deltaTime;
 		transform.position += new Vector3(speed, verticalVelocity, 0) * Time.deltaTime;
+
+		if (isSliced)
+		{
+			if ((spriteIndex != (sprites.Length - 1)) && ((Time.time - lastSpriteUpdate) > spriteUpdateDelta))
+			{
+				lastSpriteUpdate = Time.time;
+				spriteIndex++;
+				spriteRenderer.sprite = sprites[spriteIndex];
+			}
+		}
 
 		if (transform.position.y < -1)
 		{
