ce5a7f39517e2a53bbc7ee51bc4df656ce0692a0
Viet Hoang Pham <v3pro.hn97@gmail.com>
2017-04-03 22:10:01 -0400

    Continue putting UI artwork into the project and prepare for some sounds.

---
+++ Fruit Ninja/Assets/Script/SoundManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SoundManager : MonoBehaviour 
+{
+	public static SoundManager Instance { set; get; }
+
+	public AudioSource source;
+	public AudioClip[] allSounds;
+
+	private void Awake () 
+	{
+		Instance = this;	
+		DontDestroyOnLoad(gameObject);
+	}
+	
+	public void PlaySound(int soundIndex)
+	{
+		source.Play();
+	}
+
+}
